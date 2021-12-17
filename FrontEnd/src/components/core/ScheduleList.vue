58ac885f6d42b2e0f871748d76ae2df2ecb82c7e
Juan David Sandoval <judsandovalca@unal.edu.co>
2021-12-17 02:56:53 -0500

    tablestyle

--- FrontEnd/src/components/core/ScheduleList.vue
+++ FrontEnd/src/components/core/ScheduleList.vue
@@ -59,5 +59,21 @@ export default {
 </script>
 
 <style>
+table {
+  font-family: arial, sans-serif;
+  border-collapse: collapse;
+  width: 100%;
+}
+
+td, th {
+  border: 1px solid #dddddd;
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even) {
+  background-color: #dddddd;
+}
+
 
 </style>
